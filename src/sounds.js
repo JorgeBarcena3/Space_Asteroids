81e420dd4fc691d20a6ddc72f69c884053b2e281
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-05-25 20:50:35 +0200

    cambios mensaje death

--- src/sounds.js
+++ src/sounds.js
@@ -3,5 +3,5 @@ var LifePwup = new Audio("./assets/LifePwup.mp3");
 var SpeedPwup = new Audio("./assets/speedPwup.mp3");
 var ShootPwup = new Audio("./assets/speedPwup.mp3");
 var backgroundMusic = new Audio("./assets/background_music.mp3");
-backgroundMusic.play();
-backgroundMusic.volume = 0.50;
+// backgroundMusic.play();
+// backgroundMusic.volume = 0.50;
